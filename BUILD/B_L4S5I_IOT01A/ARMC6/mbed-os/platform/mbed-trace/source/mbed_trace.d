987fbaf9a2b80f1d766e0730aca7c1884c7731d8
Charlee <charlie020773@gmail.com>
2023-05-24 06:51:51 +0800

    updated

---
+++ BUILD/B_L4S5I_IOT01A/ARMC6/mbed-os/platform/mbed-trace/source/mbed_trace.d
@@ -0,0 +1,4 @@
+BUILD\B_L4S5I_IOT01A\ARMC6\mbed-os\platform\mbed-trace\source\mbed_trace.o: \
+  mbed-os\platform\mbed-trace\source\mbed_trace.c \
+  BUILD\B_L4S5I_IOT01A\ARMC6\mbed_config.h \
+  mbed-os\platform\mbed-trace\include\mbed-trace\mbed_trace.h
